ae87b7f6eeb43e54b296d78a1962b3eaa5074bda
pttrpereira <pettersonpereira2026@gmail.com>
2026-09-16 12:56:10 -0300

    server.js

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('almoco'));
+// Configura para ler os arquivos da pasta 'public ao mossar'
+app.use(express.static(path.join(__dirname, 'public ao mossar')));
+
+// Força o link principal a abrir o index.html dessa pasta
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public ao mossar', 'index.html'));
+});
+
+// Força o link /dashboard a abrir o dashboard.html dessa pasta
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public ao mossar', 'dashboard.html'));
+});
 
 io.on('connection', (socket) => {
   socket.on('resposta', (data) => {
